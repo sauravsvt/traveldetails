ddf05e813ce2b0756e8d0f532377bdbf8bb97404
Abhishek <abhishekshakya98.com@gmail.com>
2022-10-15 18:47:51 +0545

    made the page responsive

--- src/Pages/Careers.js
+++ src/Pages/Careers.js
@@ -2,38 +2,7 @@ import {useState} from 'react';
 import './Careers.css'
 
 function Careers(){
-    // const [message, setMessage] = useState('');
-    // const [error, setError] = useState(null);
-  
-    // function isValidEmail(email) {
-    //   return /\S+@\S+\.\S+/.test(email);
-    // }
-  
-    // const handleChange = event => {
-    //   if (!isValidEmail(event.target.value)) {
-    //     setError('Email is invalid');
-    //   } else {
-    //     setError(null);
-    //   }
-  
-    //   setMessage(event.target.value);
-    // };
-  
-    // return (
-    //   <div>
-    //     <input
-    //       id="message"
-    //       name="message"
-    //       value={message}
-    //       onChange={handleChange}
-    //     />
-  
-    //     {error && <h2 style={{color: 'red'}}>{error}</h2>}
-    //   </div>
-    // );
-  
 
-    
     const[email, setEmail] = useState('');
     const[error, setError] = useState('');
     const err = document.querySelector('h6');
