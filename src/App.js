c342c9a7549d89bc38aee55b83b957db96b86f54
Abhishek <abhishekshakya98.com@gmail.com>
2022-10-19 11:44:09 +0545

    some changes

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App({onClick, onToggle}) {
 
         <svg onClick={toggle} width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g clip-path="url(#clip0_6_2)">
-          <path d="M12 72H84V64H12V72ZM12 52H84V44H12V52ZM12 24V32H84V24H12Z" fill="white"/>
+          <path d="M12 72H84V64H12V72ZM12 52H84V44H12V52ZM12 24V32H84V24H12Z" fill="black"/>
           </g>
           <defs>
           <clipPath id="clip0_6_2">
